515b194dabab7744ab5143d4d5ff44b34e1afb77
jaymee821 <qjrltribdino@tip.edu.ph>
2024-04-05 11:13:47 +0800

    portfolio - updated interface

--- portfolio.php
+++ portfolio.php
@@ -8,7 +8,7 @@
         <meta content="Construction Company Website Template" name="description">
 
         <!-- Favicon -->
-        <link href="img/favicon.ico" rel="icon">
+        <link href="img/irahlogo.png" rel="icon">
 
         <!-- Google Font -->
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
@@ -31,90 +31,90 @@
         <div class="wrapper">
             <!-- Top Bar Start -->
             <div class="top-bar">
-                <div class="container-fluid">
-                    <div class="row align-items-center">
-                        <div class="col-lg-4 col-md-12">
-                            <div class="logo">
-                                <a href="index.html">
-                                    <h1>Builderz</h1>
-                                    <!-- <img src="img/logo.jpg" alt="Logo"> -->
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-lg-8 col-md-7 d-none d-lg-block">
-                            <div class="row">
-                                <div class="col-4">
-                                    <div class="top-bar-item">
-                                        <div class="top-bar-icon">
-                                            <i class="flaticon-calendar"></i>
-                                        </div>
-                                        <div class="top-bar-text">
-                                            <h3>Opening Hour</h3>
-                                            <p>Mon - Fri, 8:00 - 9:00</p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-4">
-                                    <div class="top-bar-item">
-                                        <div class="top-bar-icon">
-                                            <i class="flaticon-call"></i>
-                                        </div>
-                                        <div class="top-bar-text">
-                                            <h3>Call Us</h3>
-                                            <p>+012 345 6789</p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-4">
-                                    <div class="top-bar-item">
-                                        <div class="top-bar-icon">
-                                            <i class="flaticon-send-mail"></i>
-                                        </div>
-                                        <div class="top-bar-text">
-                                            <h3>Email Us</h3>
-                                            <p>info@example.com</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+				<div class="container-fluid">
+					<div class="row align-items-center">
+						<div class="col-lg-4 col-md-12">
+							<div class="logo">
+								<a href="index.html">
+									<img src="img/irahborder.png" alt="Logo">
+								</a>
+							</div>
+						</div>
+						<div class="col-lg-8 col-md-7 d-none d-lg-block">
+							<div class="row">
+								<div class="col-4">
+									<div class="top-bar-item">
+										<div class="top-bar-icon">
+											<i class="flaticon-calendar"></i>
+										</div>
+										<div class="top-bar-text">
+											<h3>Opening Hour</h3>
+											<p>Mon - Fri, 9:00 - 6:00</p>
+										</div>
+									</div>
+								</div>
+								<div class="col-4">
+									<div class="top-bar-item">
+										<div class="top-bar-icon">
+											<i class="flaticon-call"></i>
+										</div>
+										<div class="top-bar-text">
+											<h3>Call Us</h3>
+											<p>02-872-23581</p>
+											<p>0908-8994330</p>
+										</div>
+									</div>
+								</div>
+								<div class="col-4">
+									<div class="top-bar-item">
+										<div class="top-bar-icon">
+											<i class="flaticon-send-mail"></i>
+										</div>
+										<div class="top-bar-text">
+											<h3>Email Us</h3>
+											<p>irah@irahsolutions.com</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
             <!-- Top Bar End -->
 
             <!-- Nav Bar Start -->
             <div class="nav-bar">
-                <div class="container-fluid">
-                    <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
-                        <a href="#" class="navbar-brand">MENU</a>
-                        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
+				<div class="container-fluid">
+					<nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+						<a href="#" class="navbar-brand">MENU</a>
+						<button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+							<span class="navbar-toggler-icon"></span>
+						</button>
 
-                        <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
-                            <div class="navbar-nav mr-auto">
-                                <a href="index.html" class="nav-item nav-link">Home</a>
-                                <a href="about.html" class="nav-item nav-link">About</a>
-                                <a href="service.html" class="nav-item nav-link">Service</a>
-                                <a href="team.html" class="nav-item nav-link">Team</a>
-                                <a href="portfolio.html" class="nav-item nav-link active">Project</a>
-                                <div class="nav-item dropdown">
-                                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Pages</a>
-                                    <div class="dropdown-menu">
-                                        <a href="blog.html" class="dropdown-item">Blog Page</a>
-                                        <a href="single.html" class="dropdown-item">Single Page</a>
-                                    </div>
-                                </div>
-                                <a href="contact.html" class="nav-item nav-link">Contact</a>
-                            </div>
-                            <div class="ml-auto">
-                                <a class="btn" href="https://freewebsitecode.com">Download All Template</a>
-                            </div>
-                        </div>
-                    </nav>
-                </div>
-            </div>
+						<div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+							<div class="navbar-nav mr-auto">
+								<a href="index.php" class="nav-item nav-link">Home</a>
+								<a href="about.php" class="nav-item nav-link">About</a>
+								<a href="service.php" class="nav-item nav-link">Service</a>
+								<a href="portfolio.php" class="nav-item nav-link active">Project</a>
+								<div class="nav-item dropdown">
+									<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Pages</a>
+									<div class="dropdown-menu">
+										<a href="blog.php" class="dropdown-item">Blog Page</a>
+										<a href="single.php" class="dropdown-item">Single Page</a>
+									</div>
+								</div>
+								<a href="contact.php" class="nav-item nav-link">Contact</a>
+							</div>
+							<div class="ml-auto">
+								<a class="btn" href="#">Login</a>
+								<a class="btn" href="#">Register</a>
+							</div>
+						</div>
+					</nav>
+				</div>
+			</div>
             <!-- Nav Bar End -->
             
             
