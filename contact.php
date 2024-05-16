9ee0f357e090bb67e2c0e2b314c4329ce92a532a
jaymee821 <qjrltribdino@tip.edu.ph>
2024-05-16 16:24:28 +0800

    contact update

--- contact.php
+++ contact.php
@@ -109,10 +109,10 @@
 								</div>
 								<a href="contact.php" class="nav-item nav-link">Contact</a>
 							</div>
-							<div class="ml-auto">
+							<!-- <div class="ml-auto">
 								<a class="btn" href="login.php">Login</a>
 								<a class="btn" href="login.php">Register</a>
-							</div>
+							</div> -->
 						</div>
 					</nav>
 				</div>
@@ -282,6 +282,21 @@
 				</div>
 			</div>
 			<!-- Footer End -->
+
+			<!-- JavaScript Libraries -->
+			<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+        <script src="lib/easing/easing.min.js"></script>
+        <script src="lib/wow/wow.min.js"></script>
+        <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+        <script src="lib/isotope/isotope.pkgd.min.js"></script>
+        <script src="lib/lightbox/js/lightbox.min.js"></script>
+        <script src="lib/waypoints/waypoints.min.js"></script>
+        <script src="lib/counterup/counterup.min.js"></script>
+        <script src="lib/slick/slick.min.js"></script>
+
+        <!-- Template Javascript -->
+        <script src="js/main.js"></script>
 </body>
 
 </html>
